e321b7c362242d52e9980ff47458751ee29e02d2
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-09-05 12:52:59 -0300

    feat: screen notifications

---
+++ src/pages/Notifications/data.ts
@@ -0,0 +1,37 @@
+export default [
+  {
+    key: '1',
+    photo: 'https://api.adorable.io/avatars/1/',
+    text:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+    isOnline: true,
+    timeAgo: '1h',
+  },
+  {
+    key: '2',
+    photo: 'https://api.adorable.io/avatars/2/',
+    name: 'Mirela Souza',
+    text:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+    isOnline: false,
+    timeAgo: '5h',
+  },
+  {
+    key: '3',
+    photo: 'https://api.adorable.io/avatars/3/',
+    name: 'Marcos Henry',
+    text:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+    isOnline: true,
+    timeAgo: '1d',
+  },
+  {
+    key: '4',
+    photo: 'https://api.adorable.io/avatars/4/',
+    name: 'Túlio Furtado',
+    text:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+    isOnline: false,
+    timeAgo: '1d',
+  },
+];
--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -1,10 +1,118 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
 
-// import { Container } from './styles';
+import notifications from './data';
+
+interface PropsNotification {
+  key: string;
+  photo: string;
+  text: string;
+  isOnline: boolean;
+  timeAgo: string;
+}
+
+const Section: React.FC<PropsNotification> = (notification) => {
+  return (
+    <View style={styles.section}>
+      <View style={{position: 'relative'}}>
+        <Image source={{uri: notification.photo}} style={styles.image} />
+        {notification.isOnline && <View style={styles.isonline} />}
+      </View>
+      <View style={styles.body}>
+        <Text>{notification.text}</Text>
+      </View>
+      <View style={styles.action}>
+        <Text>...</Text>
+        <Text>{notification.timeAgo}</Text>
+      </View>
+    </View>
+  );
+};
 
 const Notifications: React.FC = () => {
-  return <View />;
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.notification}>
+        <View style={styles.header}>
+          <Text style={styles.textHeader}>Recent</Text>
+        </View>
+        {notifications.map((notification) => (
+          <Section {...notification} key={notification.key} />
+        ))}
+      </View>
+      <View style={styles.notification}>
+        <View style={styles.header}>
+          <Text style={styles.textHeader}>Earlier</Text>
+        </View>
+        {notifications.map((notification) => (
+          <Section {...notification} key={notification.key} />
+        ))}
+      </View>
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  notification: {
+    backgroundColor: '#eee',
+    marginBottom: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+  header: {
+    width: '100%',
+    padding: 15,
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 2,
+  },
+  textHeader: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#000',
+  },
+  section: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 1,
+    padding: 17,
+  },
+  image: {
+    width: 45,
+    height: 45,
+    borderRadius: 50,
+    backgroundColor: '#999',
+    marginRight: 15,
+  },
+  isonline: {
+    width: 18,
+    height: 18,
+    borderRadius: 50,
+    backgroundColor: 'green',
+    position: 'absolute',
+    bottom: 20,
+    right: 15,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  body: {
+    flex: 1,
+    paddingRight: 10,
+  },
+  action: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+});
 
 export default Notifications;
